08bf6c34e6786ab4a4c16c7cbe6bd6f3d20910a5
Daniel Fernando Castaneda Torres <dfcastanedat@unal.edu.co>
2019-05-08 00:05:54 +0000

    xddd

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,17 +1,37 @@
 class AuthorsController < ApplicationController
-    def index 
-        render json: Author.all
-    end
-    def create 
-        author = Author.new(params_author)
-        author.save
-        if author.save
-            render json: author, status: 201
-        else
-            render json: author.errors, status: :unprocesable_entity
-        end
-    end
-    def params_author
-        params.require(:author).permit(:name)
-    end
+    
+  def index
+      authors = Author.All
+      respond_to  do |format|
+          format.xml {render xml:authors, status: 200}
+          format.json {render json:authors, status: 200}
+      render json: authors
+  end
+  
+  def show
+      author = Author.find(params[:id])
+      render json: author, status:200
+  end
+  
+  def create
+      author = Author.create(params[:name], params[:age])
+      if author.save
+          render json: author, status:201
+      else
+          render json: author.errors, status: :unprocesable_entity
+      end
+  end
+  
+  def update
+      
+  end
+  
+  def destroy
+      
+  end
+  
+  def author_params
+      params.require(:author).permit(:name, :age)
+  end 
+end
 end
